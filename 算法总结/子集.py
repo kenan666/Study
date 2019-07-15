04b9628d63dbaacd9337abc90d1a4a79eab24f30
kenan666 <1746740633@qq.com>
2019-07-15 09:38:20 +0800

    子集更新

--- 算法总结/子集.py
+++ 算法总结/子集.py
@@ -20,39 +20,33 @@
 
 '''
 #思路一:库函数
-
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        for i in range(len(nums)+1):
-            for tmp in itertools.combinations(nums, i):
-                res.append(tmp)
-        return res
+def subsets(self, nums: List[int]) -> List[List[int]]:
+    res = []
+    for i in range(len(nums)+1):
+        for tmp in itertools.combinations(nums, i):
+            res.append(tmp)
+    return res
 
 
 #思路二:迭代
-
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        for i in nums:
-            res = res + [[i] + num for num in res]
-        return res
+def subsets(self, nums: List[int]) -> List[List[int]]:
+    res = [[]]
+    for i in nums:
+        res = res + [[i] + num for num in res]
+    return res
 
 
-#思路三:递归(回溯算法)
-
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        n = len(nums)
-        
-        def helper(i, tmp):
-            res.append(tmp)
-            for j in range(i, n):
-                helper(j + 1,tmp + [nums[j]] )
-        helper(0, [])
-        return res  
+#思路三:递归
+def subsets(self, nums: List[int]) -> List[List[int]]:
+    res = []
+    n = len(nums)
+    
+    def helper(i, tmp):
+        res.append(tmp)
+        for j in range(i, n):
+            helper(j + 1,tmp + [nums[j]] )
+    helper(0, [])
+    return res  
 
 
 '''
